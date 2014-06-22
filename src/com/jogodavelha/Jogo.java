3dddbbe251f36ba6c31cdfed43850a2b9dbe77a5
rodrigovilar <rvilar@gmail.com>
2014-06-22 20:38:38 -0300

    Versão inicial do Jogo da Velha

---
+++ src/com/jogodavelha/Jogo.java
@@ -0,0 +1,5 @@
+package com.jogodavelha;
+
+public class Jogo {
+
+}
---
+++ src/com/jogodavelha/JogoTest.java
@@ -0,0 +1,14 @@
+package com.jogodavelha;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JogoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
